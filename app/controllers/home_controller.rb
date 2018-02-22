cde62a8b1c94a228c04e7dbb0e335db760e2581b
Feña Agar <ferliagno@gmail.com>
2018-02-22 16:46:11 +0100

    Fixed controller to work with model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
     @status = []
-    SERVER_LIST.each do |server|
-      sv_status       = Q2ServerQuery::Client.new(server[:host], server[:port]).status
-      address         = server[:host]
+    Server.all.each do |server|
+      sv_status       = Q2ServerQuery::Client.new(server[:address], server[:port]).status
+      address         = server[:address]
       snaked_hostname = sv_status[:hostname].underscore.delete(" ")
 
       @status << sv_status.merge(address: address, snaked_hostname: snaked_hostname).with_indifferent_access
