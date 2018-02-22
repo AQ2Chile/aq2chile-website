6f6971a89d9b1d0e5f28fc67722cd5b69eeab912
Feña Agar <fernando.agar@gmail.com>
2018-02-23 00:17:48 +0100

    Servers refactor

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,5 +1,24 @@
 class Server < ApplicationRecord
 
+  def update_info(sv_status)
+    self.name     = sv_status[:hostname]
+    self.country  = GeoIp.geolocation(address, precision: :country).try(:[], :country_code)
+    self.game     = sv_status[:gamename]
+    self.save
+  end
+
+  def self.update_servers_list
+    sv_list = self.get_q2servers_list
+
+    sv_list.each do |sv|
+      sv_status = Q2ServerQuery::Client.new(sv[:address], sv[:port]).status
+      server    = find_or_create_by!(address: sv[:address], port: sv[:port])
+
+      server.update_info(sv_status)
+      sleep(1) # Because geo_ip allows 1 req per second.
+    end
+  end
+
   def self.get_q2servers_list
     uri         = URI.parse "http://q2servers.com/?mod=action&raw=1"
     res         = Net::HTTP.get_response uri
@@ -12,26 +31,6 @@ class Server < ApplicationRecord
         address: sv[0],
         port:    sv[1].to_i
       }.with_indifferent_access
-    end
-  end
-
-  def self.update_servers_list
-    sv_list = self.get_q2servers_list
-
-    sv_list.each do |sv|
-      sleep(2)
-      client    = Q2ServerQuery::Client.new(sv[:address], sv[:port])
-      sv_status = client.status
-      server    = self.find_or_initialize_by(address: sv[:address], port: sv[:port])
-
-      server.name     = sv_status[:hostname]
-      server.address  = sv[:address]
-      server.port     = sv_status[:port] || sv[:port]
-      server.country  = GeoIp.geolocation(sv[:address], precision: :country).try(:[], :country_code)
-      server.game     = sv_status[:gamename]
-
-      Rails.logger.info "Trying to save server #{sv[:address]}:#{sv[:port]}"
-      server.save
     end
   end
 end
